43b3b15c6a62baab13322ea3a1d6c10ca88bdf27
Mathieu Manta <mantam@users.sourceforge.net>
2004-07-29 09:08:35 +0000

    Launch browser to read help files with OpenURL, a new multi-platorm function

--- tcl/About.tcl
+++ tcl/About.tcl
@@ -63,25 +63,9 @@ proc ViewHelp {{name "Index"}} {
       set arr(Index) [file join [pwd] $v(path,doc) Index.html]
       set dir [file join [pwd] $v(path,doc) "en"]
    }
-   # First try to launch Internet Explorer or Netscape first
+
    set url [file join $dir $arr($name)]
-   if {$::tcl_platform(os) == "Darwin"} {
-     exec open $url
-   } elseif {[catch {
-      exec iexplore $url &
-   }] && [catch {
-	exec "C:\\Program Files\\Internet Explorer\\IEXPLORE.EXE" $url	
-   }] && [catch {
-      exec netscape -remote "openFile ($url)"
-   }] && [catch {
-      exec netscape $url &
-   }] && [catch {
-      exec mozilla $url &
-   }]} {
-      # Switch back to a page without frames for the Tcl HTML viewer
-      set arr(Index) "about.html"
-      ViewFile [file join $dir $arr($name)] [list "Index" [file join $dir $arr(Index)]]
-   }
+   OpenURL file:$url
 }
 
 #######################################################################
@@ -180,4 +164,90 @@ proc HMset_image {win handle src} {
    }
 }
 
+proc OpenURL { URL } {
+    #
+    # Job    Open an url with the default browser
+    #	     If it doesn't work, it asks the user to define the new default browser
+    #        If you want to open a file, add file: prefix to URL (ex: file:c:\tmp\try.html)
+    #
+    # In     A valid URL
+    # Out
+    # Modify If v(browser) is not set, it is set to the default browser (except on Mac)
+    #
+    # Mathieu MANTA  - DGA
+    # V 1.0
+    # July 27, 2004
+    #
+    global v
+    
+    if {$::tcl_platform(os) == "Darwin"} {
+        if { $v(browser) == ""} { exec open $URL }
+        else { exec $v(browser) $URL }
+    }
+    
+    if {$::tcl_platform(os) == "Linux"} {
+        if { $v(browser) == ""} {
+            if { [catch {exec mozilla $URL}] == 0 } { set v(browser) mozilla }
+            elseif { [catch {exec firefox $URL}] == 0 } {set v(browser) firefox }
+            else {
+                tk_messageBox -type ok -icon error -message [format [Local "Please define your default browser."] ]
+                set v(browser) [SelectBrowser]
+            }
+        }
+        else {
+            catch { exec $v(browser] $URL }
+        }
+    }
+    
+    if {$::tcl_platform(platform) == "windows"} {
+        if { $v(browser) == ""} {
+            set v(browser) [FindWinDefaultBrowser]
+        }
+        catch { exec $v(browser) "$URL" }
+    }
+}
+
+
+
+proc FindWinDefaultBrowser {} {
+    #
+    # Job    Find Windows default browser in the registry
+    #
+    # In
+    # Out    Path to the default browser
+    # Modify
+    #
+    # Mathieu MANTA  - DGA
+    # V 1.0
+    # July 27, 2004
+    #
+    set BrowserKey [registry get HKEY_CLASSES_ROOT\\http\\shell\\open\\command ""]
+    
+    if { [string equal [string index [string trim $BrowserKey " "] 0] "\""]} {
+        set BrowserPath [string trimleft $BrowserKey "\""]
+        set BrowserPath [string range $BrowserPath 0 [expr { [string first "\"" $BrowserPath ]-1}]]
+        
+    } else {
+        set BrowserPath [string range $BrowserKey 0 [string first " " $BrowserKey ]]
+    }
+    return $BrowserPath
+}
+
+proc SelectBrowser {} {
+    #
+    # Job    Select default browser through selection box
+    #
+    # In
+    # Out    Return the path to the file selected
+    #
+    # Mathieu MANTA  - DGA
+    # V 1.0
+    # July 27, 2004
+    #
+    if [catch {SaveIfNeeded} err] return
+    
+    set name [tk_getOpenFile -title [Local "Select your default browser"]]
+    
+    return $name
+}
 
