1cb114d98b8168140be47e88438954a7a1fa6512
Mathieu Manta <mantam@users.sourceforge.net>
2004-11-24 08:52:58 +0000

    Removing html_library

--- contrib/html_library-0.3/pkgIndex.tcl
+++
@@ -1,3 +0,0 @@
-# This file added to original distribution for inclusion into Transcriber
-package ifneeded html_library 0.3 "source [file join $dir html_library.tcl]; package provide html_library 0.3"
-
